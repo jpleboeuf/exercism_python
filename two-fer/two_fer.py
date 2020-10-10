8e10120cdc3a1fabbb92308e73152a6e4bcb6a09
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-10 03:45:22 +0200

    Solve exercise "Two Fer"

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,16 @@
-def two_fer(name):
-    pass
+import sys
+
+def two_fer(name:str = "you") -> str:
+    return f"One for {name}, one for me."
+
+def main():
+    argn = len(sys.argv)
+    if argn == 1:
+        print(two_fer())
+    elif argn == 2:
+        print(two_fer(sys.argv[1]))
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} [name]")
+
+if __name__ == '__main__':
+    main()
