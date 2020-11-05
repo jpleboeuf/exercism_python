2ce9d3644851c484884b4cc57dfe2c950ddf9f5a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-11-05 23:01:27 +0100

    Solve exercise "High Scores"

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,10 +1,35 @@
-def latest(scores):
-    pass
+import sys
+from typing import  List
 
 
-def personal_best(scores):
-    pass
+def latest(scores: List[int]) -> int:
+    return scores[-1]
 
 
-def personal_top_three(scores):
-    pass
+def personal_best(scores: List[int]) -> int:
+    return sorted(scores, reverse=True)[0]
+
+
+def personal_top_three(scores: List[int]) -> List[int]:
+    return sorted(scores, reverse=True)[0:3]
+
+
+def main():
+
+    def is_intstring(s:str) -> bool:
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+    
+    argn = len(sys.argv)-1
+    argv = sys.argv[1:]
+    if argn >= 1 and all(is_intstring(s) for s in argv):
+        lst:List[int] = [int(s) for s in argv]
+        print(lst)
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} int...")
+
+if __name__ == '__main__':
+    main()
