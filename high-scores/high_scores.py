3844c15da620e245c47d0fda77c26e998add490f
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-11-17 21:14:06 +0100

    Solve exercise "High Scores" (cont.)
    Changed variable naming to consider `argc`/`argv` immutable,
     using `argc_cmd`/`argv_cmd`
     for their version without the original first argument (name of the script).
    Expanded the main code to demo all functions.
    Added a module docstring.

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,3 +1,8 @@
+"""
+   This module offers a solution to
+    the "High Scores" exercise on Exercism.io.
+"""
+
 import sys
 from typing import List
 
@@ -22,12 +27,15 @@ def main():
             return True
         except ValueError:
             return False
-    
-    argn = len(sys.argv)-1
-    argv = sys.argv[1:]
-    if argn >= 1 and all(is_intstring(s) for s in argv):
-        lst:List[int] = [int(s) for s in argv]
-        print(lst)
+
+    argn_cmd = len(sys.argv)-1
+    argv_cmd = sys.argv[1:]
+    if argn_cmd >= 1 and all(is_intstring(s) for s in argv_cmd):
+        lst:List[int] = [int(s) for s in argv_cmd]
+        print(f"scores: {lst}")
+        print(f"latest: {latest(lst)}")
+        print(f"personal best: {personal_best(lst)}")
+        print(f"personal top-three: {personal_top_three(lst)}")
     else:
         raise SystemExit(f"Usage: {sys.argv[0]} int...")
 
