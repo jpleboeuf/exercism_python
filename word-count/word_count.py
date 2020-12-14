98e7b22bab33f561312110fb254ada14e1a8d642
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-12-14 15:49:05 +0100

    Solve exercise "Word Count" (cont.)
    Finally found a nice Pythonic solution.

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -5,43 +5,21 @@
 
 import sys
 
+import string
 from typing import Dict as DictType
 from collections import Counter
 from typing import Counter as CounterType
 
-from collections import namedtuple
-from types import SimpleNamespace
-
 
 def count_words(sentence:str) -> DictType[str, int]:
 
-    CursorWindow = namedtuple('CursorWindow', ['prev', 'curr', 'next'])
-
-    def is_ja(cw:CursorWindow) -> bool:
-        """is the current char a joining apostrophe?"""
-        return cw.prev.isalnum() and cw.curr == "'" and cw.next.isalnum()
-
     word_count:CounterType[str] = Counter()
-    cursor = SimpleNamespace()
-    cursor.pos = -1
-    word = SimpleNamespace()
-    word.w = ""
-    word.built = False
-    while cursor.pos+1 < (len_s := len(sentence)):
-        cursor.pos += 1
-        cursor.win = CursorWindow(*[\
-                sentence[cursor.pos-1] if cursor.pos-1 >= 0    else 'BEGIN>',\
-                sentence[cursor.pos],\
-                sentence[cursor.pos+1] if cursor.pos+1 < len_s else '<END'\
-            ])
-        if cursor.win.curr.isalnum() or is_ja(cursor.win):
-            word.w += cursor.win.curr.lower()
-        else:
-            if word.w: word.built = True
-        if word.built or (word.w and cursor.win.next == '<END'):
-            word_count.update([word.w])
-            word.w = ""
-            word.built = False
+    punctuation = string.punctuation.replace("'", "")
+    sentence_cleaned = sentence.translate(\
+            str.maketrans(punctuation, " " * len(punctuation)))
+    for word in [w_stripped.lower()\
+            for w in sentence_cleaned.split() if (w_stripped := w.strip("'"))]:
+        word_count.update([word])
     return dict(word_count)
 
 
