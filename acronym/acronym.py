0f46733066f15b3cca2c8d8cf72025ca9af2d235
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-12 22:11:13 +0100

    Solve exercise "Acronym"

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,26 @@
-def abbreviate(words):
-    pass
+"""
+   This module offers a solution to
+    the "Acronym" exercise on Exercism.io.
+"""
+
+import sys
+from typing import List
+
+
+def abbreviate(words:str) -> str:
+    real_words: List[str] = "".join(
+            [c if (c.isalpha() or c == "'") else " " for c in words]
+        ).split()
+    return "".join([w[0].upper() for w in real_words])
+
+
+def main():
+    argn_cmd = len(sys.argv)-1
+    argv_cmd = sys.argv[1:]
+    if argn_cmd == 1:
+        print(f"{abbreviate(argv_cmd[0])}")
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} words")
+
+if __name__ == '__main__':
+    main()
