e505a2734c21caa33076c185674a3317d39d0efa
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-05-18 18:57:12 +0200

    Solve exercise "Grade School"

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,22 @@
+from operator import attrgetter
+
 class School:
+
+    class Student:
+
+        def __init__(self, name, grade):
+            self.name = name
+            self.grade = grade
+
     def __init__(self):
-        pass
+        self.students = []
 
     def add_student(self, name, grade):
-        pass
+        self.students.append(School.Student(name, grade))
+        self.students.sort(key=attrgetter('grade', 'name'))
 
     def roster(self):
-        pass
+        return [s.name for s in self.students]
 
     def grade(self, grade_number):
-        pass
+        return [s.name for s in self.students if s.grade==grade_number]
