16bd56a8a0dc4a5d096782b003cfaa4654d4f2c8
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-05-24 10:10:11 +0200

    Solve exercise "Grade School" (cont.)
    Added missing type hints.

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -3,6 +3,7 @@
     the "Grade School" exercise on Exercism.io.
 """
 
+from typing import List
 from operator import attrgetter
 
 class School:
@@ -17,19 +18,19 @@ class School:
             This class defines the properties attached to a student.
         """
 
-        def __init__(self, name, grade):
-            self.name = name
-            self.grade = grade
+        def __init__(self, name:str, grade:int):
+            self.name: str = name
+            self.grade: int = grade
 
     def __init__(self):
-        self.students = []
+        self.students:List[School.Student] = []
 
-    def add_student(self, name, grade):
+    def add_student(self, name:str, grade:int):
         self.students.append(School.Student(name, grade))
         self.students.sort(key=attrgetter('grade', 'name'))
 
-    def roster(self):
+    def roster(self) -> List[str]:
         return [s.name for s in self.students]
 
-    def grade(self, grade_number):
+    def grade(self, grade_number) -> List[str]:
         return [s.name for s in self.students if s.grade==grade_number]
