8ffa7e51e1deeb91dc62f5a6b107b29daaa9376b
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-06-14 13:32:14 +0200

    Solve exercise "Clock"

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,23 @@
+from __future__ import annotations
+
+
 class Clock:
-    def __init__(self, hour, minute):
-        pass
+    def __init__(self, hour:int, minute:int):
+        hour_carry, self.minute = divmod(minute, 60)
+        day_carry, self.hour = divmod(hour + hour_carry, 24)
 
     def __repr__(self):
-        pass
+        return f"{self.hour:02}:{self.minute:02}"
 
-    def __eq__(self, other):
-        pass
+    def __eq__(self, other:Clock):
+        return (self.hour == other.hour) and (self.minute == other.minute)
 
-    def __add__(self, minutes):
-        pass
+    def __add__(self, minutes:int):
+        hour_carry, minute = divmod(self.minute + minutes, 60)
+        day_carry, hour = divmod(self.hour + hour_carry, 24)
+        return Clock(hour, minute)
 
-    def __sub__(self, minutes):
-        pass
+    def __sub__(self, minutes:int):
+        hour_uncarry, minute = divmod(self.minute - minutes, -60)
+        day_uncarry, hour = divmod(self.hour - hour_uncarry, -24)
+        return Clock(hour, minute)
