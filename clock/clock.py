7303847ebcbe1ef7bd28fab1019b9ca4cbb67db0
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-06-15 10:55:33 +0200

    Solve exercise "Clock" (cont.)
    Added the missing main function,
     and some missing docstring-s.

--- clock/clock.py
+++ clock/clock.py
@@ -1,7 +1,19 @@
+"""
+   This module offers a solution to
+    the "Clock" exercise on Exercism.io.
+"""
+
 from __future__ import annotations
+import sys
 
 
 class Clock:
+    """
+        This class encapsulates a solution to the exercise:
+         the initialization of the class,
+         and the methods providing the solution.
+    """
+
     def __init__(self, hour:int, minute:int):
         hour_carry, self.minute = divmod(minute, 60)
         day_carry, self.hour = divmod(hour + hour_carry, 24)
@@ -21,3 +33,26 @@ class Clock:
         hour_uncarry, minute = divmod(self.minute - minutes, -60)
         day_uncarry, hour = divmod(self.hour - hour_uncarry, -24)
         return Clock(hour, minute)
+
+
+def main():
+    argn_cmd = len(sys.argv)-1
+    argv_cmd = sys.argv[1:]
+
+    try:
+        if argn_cmd >= 1:
+            clock = Clock(*(int(i) for i in argv_cmd[0].split(",")))
+            if argn_cmd == 1:
+                print(clock)
+            elif argn_cmd == 2:
+                if argv_cmd[1][0] in ("+", "-"):
+                    print(clock + int(argv_cmd[1]))
+                else:
+                    raise SystemExit(f"Usage: {sys.argv[0]} H,M [+|-m]")
+        else:
+            raise SystemExit(f"Usage: {sys.argv[0]} H,M [+|-m]")
+    except ValueError as ve:
+        print("Problem encountered with some values:\n", ve)
+
+if __name__ == '__main__':
+    main()
